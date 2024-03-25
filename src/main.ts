9189abed122ee9f306cfa262d87ea1095fa4191f
anthony-mini <anthony-mini@outlook.fr>
2024-03-25 22:54:27 +0100

    Ajouter une gestion d'erreur lors du démarrage de l'application

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 import { config } from 'dotenv';
 
 config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(+process.env.PORT || 3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(+process.env.PORT || 3000);
+  } catch (error) {
+    Logger.error(
+      `Failed to bootstrap the application`,
+      error.stack,
+      'Bootstrap',
+    );
+  }
 }
 bootstrap();
