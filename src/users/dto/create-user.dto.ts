f4670714f16f24b4cde38cbf134ff3cbfed2e78d
anthony-mini <anthony-mini@outlook.fr>
2024-05-09 16:26:09 +0200

    chore: Adding Cors Origin security. Bind front w/ backend

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,22 +2,23 @@ import { IsEmail, IsNotEmpty, IsDefined, IsEnum } from 'class-validator';
 import { Role } from '../enums/role.enum';
 
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'Last name is required' })
+  lastName: string;
+
+  @IsNotEmpty({ message: 'First name is required' })
+  firstName: string;
+
   @IsEmail({}, { message: 'Invalid email format' })
   email: string;
 
   @IsNotEmpty({ message: 'Password is required' })
   password: string;
 
-  @IsNotEmpty({ message: 'First name is required' })
-  firstName: string;
-
-  @IsNotEmpty({ message: 'Last name is required' })
-  lastName: string;
-
-  @IsNotEmpty({ message: 'Phone number is required' })
-  phone: string;
-
   @IsDefined()
   @IsEnum(Role, { message: 'Invalid role specified' })
   role: Role;
+
+  phone: string;
+
+  address: string;
 }
