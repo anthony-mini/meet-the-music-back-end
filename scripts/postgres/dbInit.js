eb55146e6fcef2357fea5c9941344d025840e7bd
anthony-mini <anthony-mini@outlook.fr>
2024-05-20 19:37:52 +0200

    fix: script sql

--- scripts/postgres/dbInit.js
+++ scripts/postgres/dbInit.js
@@ -2,12 +2,27 @@
 require('dotenv').config();
 const { Client } = require('pg');
 
+const getSSLConfig = () => {
+  const env = process.env.NODE_ENV;
+  let rejectUnauthorized = false;
+
+  if (env === 'staging' || env === 'production') {
+    rejectUnauthorized = true;
+  }
+
+  return {
+    rejectUnauthorized,
+    ca: process.env.CA_CERT,
+  };
+};
+
 const client = new Client({
   host: process.env.DB_HOST,
   port: +process.env.DB_PORT,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: getSSLConfig(),
 });
 
 const query = `
@@ -16,6 +31,10 @@ CREATE SCHEMA IF NOT EXISTS app;`;
 client.connect();
 
 client.query(query, (err, res) => {
-  console.log(err, res);
+  if (err) {
+    console.error('Error executing query', err.stack);
+  } else {
+    console.log('Query result:', res);
+  }
   client.end();
 });
