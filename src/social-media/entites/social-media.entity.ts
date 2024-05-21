e778d1a5fcff3d78e2c08318c6babb65493d498e
anthony-mini <anthony-mini@outlook.fr>
2024-05-21 09:09:52 +0200

    data: improve table socialMedia

--- src/social-media/entites/social-media.entity.ts
+++ src/social-media/entites/social-media.entity.ts
@@ -1,20 +1,40 @@
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 import { ArtistProfile } from '../../artist-profile/entities/artist-profile.entity';
+import { SocialMediaName } from '../enums/socialMediaName.enum';
 
+/**
+ * Composite key for the SocialMedia entity that includes the socialMediaName and artistProfileId.
+ */
 @Entity('socialMedia')
 export class SocialMedia {
-  @PrimaryGeneratedColumn({ name: 'id' })
-  id: number;
+  @Column({
+    name: 'socialMediaName',
+    type: 'enum',
+    enum: [
+      SocialMediaName.INSTAGRAM,
+      SocialMediaName.FACEBOOK,
+      SocialMediaName.TWITTER,
+      SocialMediaName.YOUTUBE,
+      SocialMediaName.LINKEDIN,
+      SocialMediaName.TIKTOK,
+      SocialMediaName.SNAPCHAT,
+      SocialMediaName.WHATSAPP,
+      SocialMediaName.DISCORD,
+      SocialMediaName.SPOTIFY,
+      SocialMediaName.SOUNDCLOUD,
+    ],
+  })
+  @PrimaryColumn()
+  socialMediaName: SocialMediaName;
 
   @ManyToOne(() => ArtistProfile, (artistProfile) => artistProfile.socialMedia)
   @JoinColumn({ name: 'artistProfileId' })
   artistProfile: ArtistProfile;
+  @PrimaryColumn()
+  artistProfileId: number;
+
+  @Column({ name: 'url' })
+  url: string;
 
   @Column({
     name: 'createdAt',
