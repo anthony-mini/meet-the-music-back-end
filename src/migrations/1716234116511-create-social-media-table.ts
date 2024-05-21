222fdf0a3171e366e551d3cda7c67325a5c319ed
anthony-mini <anthony-mini@outlook.fr>
2024-05-21 09:09:24 +0200

    data: improve socialMedia Table.
    Add composite key

--- src/migrations/1716234116511-create-social-media-table.ts
+++ src/migrations/1716234116511-create-social-media-table.ts
@@ -13,18 +13,30 @@ export class CreateSocialMediaTable1716234116511 implements MigrationInterface {
         schema: 'app',
         columns: [
           {
-            name: 'id',
-            type: 'int',
-            isPrimary: true,
-            isGenerated: true,
-            generationStrategy: 'increment',
-          },
-          {
             name: 'artistProfileId',
             type: 'int',
+            isPrimary: true,
           },
           {
-            name: 'name',
+            name: 'socialMediaName',
+            type: 'enum',
+            enum: [
+              'INSTAGRAM',
+              'FACEBOOK',
+              'TWITTER',
+              'YOUTUBE',
+              'LINKEDIN',
+              'TIKTOK',
+              'SNAPCHAT',
+              'WHATSAPP',
+              'DISCORD',
+              'SPOTIFY',
+              'SOUNDCLOUD',
+            ],
+            isPrimary: true,
+          },
+          {
+            name: 'url',
             type: 'varchar',
             isNullable: true,
           },
@@ -37,13 +49,13 @@ export class CreateSocialMediaTable1716234116511 implements MigrationInterface {
             name: 'updatedAt',
             type: 'timestamp',
             default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
           },
         ],
       }),
     );
 
     // Add foreign key constraint from socialMedia to artistProfile
-
     await queryRunner.createForeignKey(
       'app.socialMedia',
       new TableForeignKey({
